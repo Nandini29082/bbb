f65dde46bf34096ca5c219f5913d11460ca2fe96
Nandini29082 <142983769+Nandini29082@users.noreply.github.com>
2023-10-12 16:53:35 +0000

    basics

---
+++ basics/datatypes.js
@@ -0,0 +1,49 @@
+"use strict";//treate all js code as newer version
+console.log(3+3)//code readability should be high
+console.log("Hitesh")
+ 
+let name="hitesh"
+let age=18
+let isLoggedin=false
+let state;
+
+//number=>2 to power 53
+//bigint
+//string=>""
+//boolen=>true/false
+//null=>standalone value
+//undefined=>
+//symbol=>uniqe
+
+//object
+
+console.log(typeof undefined);//undefined
+console.log(typeof null);//object
+console.log(typeof "name")
+console.log(typeof age)
+
+
+
+
+let score="nandu"
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber=Number(score)
+//console.log(typeof valueinnumber);
+//console.log(valueinnumber);
+
+
+//"33"=>33
+//"33abc"=>NaN
+//true=>1;false=>0
+let isloggedin="hitesh"
+let booleanIsLoggedIn=Boolean(isLoggedin)
+//console.log(boolenisloggedin);
+//1=>true;0=>false
+//""=>false
+//"nandu"=>true
+let somenumber=33
+let stringnumber=String(somenumber)
+//console.log(stringnumber);
+//console.log(typepf stringnumber);
